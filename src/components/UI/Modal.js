db5f0ad89998e88e58317c8de0d0d18f488fc5a3
apriori-yarik <yarik2803chess@yandex.ru>
2022-03-06 20:44:37 +0200

    show cart functionality added

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClick}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -16,7 +16,10 @@ const Modal = (props) => {
 
   return (
     <>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onHideCart} />,
+        portalElement
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
